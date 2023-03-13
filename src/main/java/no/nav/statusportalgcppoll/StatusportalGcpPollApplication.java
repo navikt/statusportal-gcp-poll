be09c39c0611b61f20d6523a9bf3832aad07f6a5
larsLotNav <lars.august.lotsberg@nav.no>
2023-03-13 09:08:40 +0100

    Initial commit

---
+++ src/main/java/no/nav/statusportalgcppoll/StatusportalGcpPollApplication.java
@@ -0,0 +1,13 @@
+package no.nav.statusportalgcppoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusportalGcpPollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatusportalGcpPollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/nav/statusportalgcppoll/StatusportalGcpPollApplicationTests.java
@@ -0,0 +1,13 @@
+package no.nav.statusportalgcppoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatusportalGcpPollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
