d025b0d0ad933e8d3b2b748a0d6d75c61ea72642
larsLotNav <lars.august.lotsberg@nav.no>
2023-08-09 09:46:48 +0200

    Added source to record

--- src/main/java/no/nav/statusportalgcppoll/RecordDto.java
+++ src/main/java/no/nav/statusportalgcppoll/RecordDto.java
@@ -24,6 +24,8 @@ public class RecordDto {
 
     private StatusDto status = null;
 
+    private RecordSourceDto source = null;
+
     private String description = null;
 
     private String logLink = null;
@@ -31,7 +33,14 @@ public class RecordDto {
     private Integer responseTime = null;
 
 
+    public RecordSourceDto getSource() {
+        return source;
+    }
 
+    public RecordDto setSource(RecordSourceDto source) {
+        this.source = source;
+        return this;
+    }
 
     public OffsetDateTime getTimestamp() {
         return timestamp;
@@ -180,6 +189,8 @@ public class RecordDto {
             writer.value(recordDto.getLogLink());
             writer.name("responseTime");
             writer.value(recordDto.getResponseTime());
+            writer.name("source");
+            writer.value(RecordSourceDto.GCP_POLL.toString());
             writer.endObject();
         }
 
